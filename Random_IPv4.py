a9aef43cb0fb32a4d0b496205da5096c76c273bb
HuMuHuMu <15821175224@139.com>
2022-03-07 21:27:13 +0800

    Init commit

---
+++ Random_IPv4.py
@@ -0,0 +1,7 @@
+import random
+a = random.randint(1, 255)
+b = random.randint(0, 255)
+c = random.randint(0, 255)
+d = random.randint(0, 255)
+random_IP = str(a) + '.' + str(b) + '.' + str(c) + '.' + str(d)
+print('Random_IPv4 = ', random_IP)
